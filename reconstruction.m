2e6efc40e1f54e5507aeef7af3d434e2b2eb311d
Huang15 <99675139+Huang15@users.noreply.github.com>
2023-07-09 18:39:22 +0200

    Add files via upload

--- reconstruction.m
+++ reconstruction.m
@@ -1,6 +1,13 @@
-imageDir = fullfile('C:\Users\HUANG\Desktop\TUM\CV\project\kicker\images\dslr_images_undistorted');
-% imageDir = fullfile('C:\Users\HUANG\Desktop\TUM\CV\project\kicker\images\dslr_images_undistorted');
+clc; clear;
+
+%% Given an image folder
+
+% imageDir = fullfile('C:\Users\HUANG\Desktop\TUM\CV\project\delivery_area\images\dslr_images_undistorted');
+imageDir = fullfile('C:\Users\HUANG\Desktop\TUM\CV\project\kicker\images\dslr_images_undistorted');%Please change
 imds = imageDatastore(imageDir);
+
+
+%% Initialization
 
 images = cell(1, numel(imds.Files));
 num_img =numel(imds.Files);
@@ -8,26 +15,27 @@ Points =cell(1, num_img);
 Features=cell(1, num_img);
 gray_img=cell(1, num_img);
 
+%% Loading and pre-processing images
 
 for i = 1:num_img
     I = readimage(imds, i);
     gray_img{i}= medfilt2(im2gray(I));
-    
 end
 
 
-
-% imageSize =[4135 6208];
-% focalLength =[3408.59 3408.87];
-% principalPoint=[3117.24 2064.07];
-
+%% Giving the camera inside reference
+%Please change yourself
 imageSize =[4137 6211];
 focalLength =[3410.34 3409.98];
 principalPoint=[3121.33 2067.07];
 intrinsics =  cameraIntrinsics(focalLength,principalPoint,imageSize);
+%% Feature finding, matching, triangulation
 
+% Give the features of the first picture
 Points{1}= detectSIFTFeatures(gray_img{1});
 [Features{1}, Points{1}]= extractFeatures(gray_img{1}, Points{1});
+
+% Create an ensemble to store the features, matches, and camera positions for each image
 
 vSet = imageviewset;
 viewId = 1;
@@ -37,83 +45,91 @@ for i = 2:numel(images)
     Points{i}= detectSIFTFeatures(gray_img{i});
     [Features{i}, Points{i}]= extractFeatures(gray_img{i}, Points{i});
     max_Pairs=0;
+    % Match the j graph with the previous 1~j-1 graphs and find the pair with the highest match
     for j =1:i-1
         indexPairs  = matchFeatures(Features{i-j}, Features{i});
-
         if numel(indexPairs) >max_Pairs
             max_Pairs=numel(indexPairs);
             k=j;
             indexPairs_k=indexPairs;
         end
-
-
     end
-    
+    % Select matched points.
     matchedPoints1 = Points{i-k}(indexPairs_k(:, 1));
     matchedPoints2 = Points{i}(indexPairs_k(:, 2));
 
-
-    [E, inlierIdx] = estimateEssentialMatrix(matchedPoints1.Location, matchedPoints2.Location,...
-        intrinsics);
-
+    % estimateFundamentalMatrix
+    [F, inlierIdx] = estimateFundamentalMatrix(matchedPoints1.Location, matchedPoints2.Location, Method="RANSAC", NumTrials=5000, DistanceThreshold=1e-4);
+    E =intrinsics.K'*F*intrinsics.K;
     inlierPoints1 = matchedPoints1.Location(inlierIdx, :);
     inlierPoints2 = matchedPoints2.Location(inlierIdx, :);  
     
+    % Estimate the camera pose of current view relative to the previous view.
     [relPose, validPointFraction] = ...
     estrelpose(E, intrinsics, inlierPoints1(1:2:end, :),...
     inlierPoints2(1:2:end, :));
-          
+    
+    % Get the table containing the previous camera pose.
     prevPose = poses(vSet, i-k).AbsolutePose;
     currPose = rigidtform3d(prevPose.A*relPose.A);
     
+    % Add the current view to the view set.
     vSet = addView(vSet, i, currPose, Points=Points{i});
-
+    
+    % Store the point matches between the previous and the current views.
     vSet = addConnection(vSet, i-k, i, relPose, Matches=indexPairs_k(inlierIdx,:));
     
     tracks = findTracks(vSet);
 
     camPoses = poses(vSet);
-
+    
+    % Triangulate initial locations for the 3-D world points.
     xyzPoints = triangulateMultiview(tracks, camPoses, intrinsics);
-
+    
+    % Refine the 3-D world points and camera poses.
     [xyzPoints, camPoses, reprojectionErrors] = bundleAdjustment(xyzPoints, ...
         tracks, camPoses, intrinsics, FixedViewId=1, ...
         PointsUndistorted=true);
-    
+    % Store the refined camera poses.
     vSet = updateView(vSet, camPoses);
 end
+%% Reconstruction shows
 
-    % Display camera poses.
-% camPoses = poses(vSet);
-% figure;
-% plotCamera(camPoses, Size=0.2);
-% hold on
+ % Display camera poses.
+camPoses = poses(vSet);
+figure;
+plotCamera(camPoses, Size=0.2);
+hold on
 
 % Exclude noisy 3-D points.
 goodIdx = (reprojectionErrors < 5);
 xyzPoints = xyzPoints(goodIdx, :);
 
 % Display the 3-D points.
-% pcshow(xyzPoints, VerticalAxis='y', VerticalAxisDir='down', MarkerSize= 45);
-% grid on
-% hold off
+pcshow(xyzPoints, VerticalAxis='y', VerticalAxisDir='down', MarkerSize= 45);
+grid on
+hold off
 
-% % Specify the viewing volume.
-% loc1 = camPoses.AbsolutePose(1).Translation;
-% xlim([loc1(1)-5, loc1(1)+4]);
-% ylim([loc1(2)-5, loc1(2)+4]);
-% zlim([loc1(3)-1, loc1(3)+20]);
-% camorbit(0, -30);
+% Specify the viewing volume.
+loc1 = camPoses.AbsolutePose(1).Translation;
+xlim([loc1(1)-5, loc1(1)+4]);
+ylim([loc1(2)-5, loc1(2)+4]);
+zlim([loc1(3)-1, loc1(3)+20]);
+camorbit(0, -30);
+
+%% Point cloud processing
 
 
 ptCloud = pointCloud(xyzPoints);
-distanceThreshold = 1.0;  % 参数，可调整，它决定了哪些点被认为是同一对象的一部分
-numClusterPoints = 100;  % 参数，可调整，它决定了一个对象最少需要多少个点
+% parameter, adjustable, which determines which points are considered to be part of the same object
+distanceThreshold = 1.0; 
+% parameter, which can be adjusted, determines the minimum number of points needed for an object
+numClusterPoints = 100;  
 
-% 使用 pcsegdist 函数分割点云
+% Splitting point clouds using the pcsegdist function
 labels = pcsegdist(ptCloud, distanceThreshold, 'NumClusterPoints', numClusterPoints);
 
-% 使用labels来分割点云
+% Using labels to segment point clouds
 segmentedClouds = cell(max(labels),1);
 for i = 1:max(labels)
     segmentedClouds{i} = select(ptCloud, labels == i);
@@ -125,35 +141,34 @@ for i = 1:numel(segmentedClouds)
     hold on;
 end
 hold off;
+% parameter that determines the minimum number of points needed to create an alpha shape
+minNumPointsForAlphaShape = 4;  
 
-minNumPointsForAlphaShape = 4;  % 参数，决定了创建 alpha shape 需要的最少点数
-
-% 定义颜色映射（可修改这个，使用不同的颜色）
+% Define the colour mapping (this can be modified to use a different colour)
 colors = jet(numel(segmentedClouds));
 
-% 创建一个新的 figure
+% Create a new figure
 figure;
 
-% 为每个簇创建一个 alpha shape，并在同一图中绘制出来
+% Create an alpha shape for each cluster and plot it on the same graph
 for i = 1:numel(segmentedClouds)
-    % 提取当前簇的点并将其转换为double类型
+    % Extracts the points of the current cluster and converts them to double type
     xyzPointsCurrent = double(segmentedClouds{i}.Location);
-    
-    % 检查点的数量
+
+    % Verify that the number of points
     if size(xyzPointsCurrent, 1) < minNumPointsForAlphaShape
         continue;
     end
-    
-    % 创建 alpha shape
+
+    % Create alpha shape
     shp = alphaShape(xyzPointsCurrent(:,1), xyzPointsCurrent(:,2), xyzPointsCurrent(:,3));
-    
-    % 绘制 alpha shape，并为它指定颜色
+
+    % Draws an alpha shape and assigns a colour to it
     plot(shp, 'FaceColor', colors(i, :), 'EdgeColor', 'k');
     hold on;
 end
 hold off;
 
-% 设置图的标题
 title('3D Alpha Shapes for All Clusters');
 
 
